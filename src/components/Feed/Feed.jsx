1992c782bf3b2909e3469d4cca3a2dee45e639f2
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-05-01 21:21:03 +0530

    adding home page images

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,7 +1,115 @@
 import React from "react";
+import './Feed.css';
+import thumbnail1 from '../../assets/thumbnail1.png';
+import thumbnail2 from '../../assets/thumbnail2.png';
+import thumbnail3 from '../../assets/thumbnail3.png';
+import thumbnail4 from '../../assets/thumbnail4.png';
+import thumbnail5 from '../../assets/thumbnail5.png';
+import thumbnail6 from '../../assets/thumbnail6.png';
+import thumbnail7 from '../../assets/thumbnail7.png';
 
-export function Feed(){
-    return(
-        <div></div>
+
+
+export function Feed() {
+    return (
+        <div className='feed' >
+            <div className='card'>
+                <img src={thumbnail1} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail2} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail3} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail4} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail5} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail6} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail7} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail1} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail1} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail2} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail3} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail4} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail5} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail6} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail7} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+            <div className='card'>
+                <img src={thumbnail1} />
+                <h2>Best channel to learn coding that help you to be a web development</h2>
+                <h3>Greatstack</h3>
+                <p>15K views &bull; 2 days ago</p>
+            </div>
+
+        </div>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Home.css';
 import { Sidebar } from "../../components/sidebar/sidebar";
+import { Feed } from "../../components/Feed/Feed";
 
 
 
@@ -11,7 +12,10 @@ export function Home({sidebar}) {
         <div>
 
           <Sidebar sidebar={sidebar}/>
-          
+          {/* here we are mounting the sidebar and feed */}
+          <div className={`container ${sidebar?"":'large-container'}`}>
+            <Feed/>
+          </div>
 
 
         </div>
