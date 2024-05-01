422591ec38b824b8fb18405a2c2d53bc6b82e0ea
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-05-02 01:32:08 +0530

    sidebar added

---
+++ src/components/Recommended/Recommended.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import './Recommended.css';
+import thumbnail1 from '../../assets/thumbnail1.png'
+import thumbnail2 from '../../assets/thumbnail2.png'
+import thumbnail3 from '../../assets/thumbnail3.png'
+import thumbnail4 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+
+
+
+export function Recommended() {
+    return (
+        <div className="recommended">
+            <div className='side-video-list'>
+                <img src={thumbnail1} />
+                <div className="video-info">
+                    <h4>Great channel that help you to be a web developer</h4>
+                    <p>GreatStack</p>
+                    <p>199 Views</p>
+                </div>
+            </div>
+            <div className='side-video-list'>
+                <img src={thumbnail2} />
+                <div className="video-info">
+                    <h4>Great channel that help you to be a web developer</h4>
+                    <p>GreatStack</p>
+                    <p>199 Views</p>
+                </div>
+            </div>
+            <div className='side-video-list'>
+                <img src={thumbnail3} />
+                <div className="video-info">
+                    <h4>Great channel that help you to be a web developer</h4>
+                    <p>GreatStack</p>
+                    <p>199 Views</p>
+                </div>
+            </div>
+            <div className='side-video-list'>
+                <img src={thumbnail4} />
+                <div className="video-info">
+                    <h4>Great channel that help you to be a web developer</h4>
+                    <p>GreatStack</p>
+                    <p>199 Views</p>
+                </div>
+            </div>
+            <div className='side-video-list'>
+                <img src={thumbnail5} />
+                <div className="video-info">
+                    <h4>Great channel that help you to be a web developer</h4>
+                    <p>GreatStack</p>
+                    <p>199 Views</p>
+                </div>
+            </div>
+            <div className='side-video-list'>
+                <img src={thumbnail6} />
+                <div className="video-info">
+                    <h4>Great channel that help you to be a web developer</h4>
+                    <p>GreatStack</p>
+                    <p>199 Views</p>
+                </div>
+               
+            </div>
+            <div className='side-video-list'>
+                    <img src={thumbnail7} />
+                    <div className="video-info">
+                        <h4>Great channel that help you to be a web developer</h4>
+                        <p>GreatStack</p>
+                        <p>199 Views</p>
+                    </div>
+                </div>
+
+        </div>
+    )
+
+}
--- src/pages/video/video.jsx
+++ src/pages/video/video.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import './video.css'
 import PlayVideo from "../../components/playVideo/playVideo";
+import { Recommended } from "../../components/Recommended/Recommended";
 export function Video(){
     return(
         <div className='play-container'>
             {/* here playvideo component are mounting */}
             <PlayVideo/>
+            {/* after that here Recommended component are mounting */}
+
+            <Recommended/>
 
         </div>
     )
